59112fbf30da1dd66a0ac5b335aaf849d1905897
Suzan YATAĞAN <136517240+syatagan@users.noreply.github.com>
2023-09-13 21:08:20 +0300

    suzan
    suzan

--- pg_show_category_products.py
+++ pg_show_category_products.py
@@ -4,6 +4,7 @@ from st_aggrid import AgGrid, GridUpdateMode, JsCode, DataReturnMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from Src.utils import *
 import pg_show_product_detail
+import time
 
 image_jscode = JsCode("""
 function(params) {
@@ -16,6 +17,13 @@ function(params) {
     };
 }
 """)
+def app(xCategory = ""):
+    if (xCategory != ""):
+        st.subheader(f"Products in {xCategory} Category")
+        df = get_category_product_list(xCategory)
+        show_product_list(df)
+    else:
+        st.error("No defined Category")
 
 def get_category_product_list(xCategory):
     df = read_food_data()
@@ -24,19 +32,17 @@ def get_category_product_list(xCategory):
     return df
 
 def go_product_detail():
-    if grd_Table['selected_rows']:
-        product_code = grd_Table['selected_rows'][0]['code']
-        st.write("code:", product_code)
-    else:
-        st.write("No row selected")
+    #st.write(st.session_state.id_row)
+    product_code = st.session_state.id_row['selectedRows'][0]['code']
+    pg_show_product_detail.app(product_code)
+
 
 def show_product_list(df):
-        #df = df.sort_values(by="product_name_en", ascending=True)
         df["img_small_url"] = df.apply(lambda x: x["url"].replace(".400.jpg", ".100.jpg"), axis=1)
         grd = GridOptionsBuilder() #.from_dataframe(df)
         grd.configure_grid_options(rowHeight=100)
         grd.configure_pagination(enabled=True, paginationPageSize=10)
-        grd.configure_selection(selection_mode="single")
+        grd.configure_selection()
         grd.configure_column(field="code", header_name="Code", width=80)
         grd.configure_column(field="product_name_en", header_name="Name", width=100)
         grd.configure_column(field="brands", header_name="Brand", width=80)
@@ -45,29 +51,26 @@ def show_product_list(df):
         grd.configure_column(field="allergens", header_name="Allergens", width=80, cellStyle={'color': 'red'})
         grd.configure_column(field="img_small_url", header_name="Image", cellStyle=image_jscode, width = 80)
         gridOptions = grd.build()
+        with st.form(key="grid_frm"):
+            st.form_submit_button(label="GO",on_click=go_product_detail)
+            grd_Table = AgGrid(data=df,
+                               gridOptions=gridOptions,
+                               fit_columns_on_grid_load=True,
+                               height=500,
+                               width="100%",
+                               allow_unsafe_jscode=True,
+                               custom_css={
+                                   "#gridToolBar": {
+                                       "padding-bottom": "0px !important"
+                                   },
+                                   ".ag-header-cell-text, .ag-cell": {
+                                       "font-size": "18px !important",
+                                   }},
+                               key="id_row",
+                               theme=AgGridTheme.STREAMLIT,
+                               reload_data=True,
+                               update_mode=GridUpdateMode.SELECTION_CHANGED,
+                               )
 
-        grd_Table = AgGrid(data=df,
-                           gridOptions=gridOptions,
-                           fit_columns_on_grid_load=True,
-                           height=500,
-                           width="100%",
-                           allow_unsafe_jscode=True,
-                           custom_css={"#gridToolBar": {"padding-bottom": "0px !important"}},
-                           theme="streamlit",
-                           key="id_row",
-                           reload_data=True,
-                           update_mode=GridUpdateMode.SELECTION_CHANGED
-                           )
-        if grd_Table['selected_rows']:
-            product_code = grd_Table['selected_rows'][0]['code']
-            st.write("code:" , product_code)
-        else:
-            st.write("No row selected")
 
-def app(xCategory = ""):
-    if (xCategory != ""):
-        st.subheader(f"Products in {xCategory} Category")
-        df = get_category_product_list(xCategory)
-        show_product_list(df)
-    else:
-        st.error("No defined Category")
+
